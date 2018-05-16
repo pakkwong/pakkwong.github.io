09a5693977760d189be86d0e674e62d164f968ee
pakkwong <37121718+pakkwong@users.noreply.github.com>
2018-05-16 05:49:11 -0700

    update

--- p5/empty-example/bouncing ball.js
+++ p5/empty-example/bouncing ball.js
@@ -1,25 +1,26 @@
-float circleX;
-float xspeed= 10;
+var circleX;
+var xspeed= 10;
 
-function setup() {
+function setup(){
   createCanvas(600,350);
   circleX=0;
 }
 
-function draw() {
-  background(200);
-  fill(255);
-  stroke(0);
-  ellipse(circleX,height/2,32,32);
-  if(mousePressed){
-  	circleX=circleX + xspeed;
-  }
-
-  if(circleX>width){
-  	xspeed=-30;
-  }
-
-  if(circleX<0){
-  	xspeed=15;
-  }
+function draw(){
+ background(200);
+ fill(255);
+ stroke(0);
+ ellipse(circleX,height/2,32,32);
+ if(mouseIsPressed){
+ circleX=circleX + xspeed;
+ }
+ 
+ if(circleX>width){
+    xspeed=-30;
+ }
+ 
+ if(circleX<0){
+   xspeed=15;
+ }
+ 
 }
